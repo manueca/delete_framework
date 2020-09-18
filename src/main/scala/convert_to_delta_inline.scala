7e6973e3fd5f12f36fe5640d465a19f0f3db301f
Jerry Cheruvathoor <jerry.cheruvathoor@nike.com>
2020-09-18 11:07:36 -0700

    added delete spark

--- src/main/scala/convert_to_delta_inline.scala
+++ src/main/scala/convert_to_delta_inline.scala
@@ -1,3 +1,4 @@
+//spark-submit --driver-memory 100G --executor-memory 47G --conf spark.delta.merge.repartitionBeforeWrite=true  --conf "spark.debug.maxToStringFields=100" --conf "spark.driver.extraJavaOptions=-Xms20g" --conf "spark.driver.maxResultSize=5g" --conf "spark.delta.merge.repartitionBeforeWrite=true"  --packages io.delta:delta-core_2.11:0.4.0 --class "convert_to_delta_inline" qaframework_2.11-1.0.11.jar s3://zz-testing/jcher2/bm_inv/
 import io.delta.tables._;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.DataFrame ;
@@ -9,8 +10,10 @@ object convert_to_delta_inline {
 	def main(args : Array[String]) : Unit = {
         var spark = SparkSession.builder().appName("delta").config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension").config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog").enableHiveSupport().getOrCreate()
 	//spark.sql("select * from dsmsca_staging.ip_bm_inventory_daily_base_t").show()
-        val treatment_partition_schema = "account_type string,supergeo_cd string,retailer_id string,snapshot_dt string"
-        DeltaTable.convertToDelta(spark,"parquet.`s3://zz-testing/jcher2/po_base/`",treatment_partition_schema)
+        val treatment_partition_schema = "account_type string,supergeo_cd string,retailer_id string,snapshot_dt string";
+	val treatment_location=args(0);
+        val treatment_identifier = String.format("parquet.`%s`",treatment_location);
+        DeltaTable.convertToDelta(spark,treatment_identifier,treatment_partition_schema)
         }
 	
 
--- src/main/scala/deletion_process.scala
+++ src/main/scala/deletion_process.scala
@@ -10,17 +10,22 @@ import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 
-object deletion_process_delta {
+object deletion_process {
 	def main(args : Array[String]) : Unit = {
         var spark = SparkSession.builder().appName("delta").config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension").config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog").enableHiveSupport().getOrCreate()
-        val treatment_s3_location = "s3://zz-testing/jcher2/po_base/";
+		print(args(0));
+		print(args(1));
+		
+                val treatment_s3_location = "s3://zz-testing/jcher2/bm_poc/";
 		val blacklist_s3_location = "s3://zz-testing/jcher2/blacklist_modified/";
-		val treatment_field = "prodt_cd";
+		val treatment_field = "prod_cd";
 		val blacklist_field = "prodt_cd";
 		val treatment_alias = "treatment"
 		val blacklist_alias = "blacklist"
 		spark.sql("set spark.databricks.delta.autoCompact.enabled = true")
-		//val delta_table_1 = spark.read.format("delta").load(treatment_s3_location)
+		val delta_table_2 = spark.read.format("delta").load(treatment_s3_location)
+		delta_table_2.printSchema()
+
 		val delta_table_1 = DeltaTable.forPath(spark, treatment_s3_location)
 	
 		//spark.sql("ANALYZE TABLE delta_table_1 COMPUTE STATISTICS FOR COLUMNS prodt_cd")
@@ -33,7 +38,7 @@ object deletion_process_delta {
 		//val condition = String.format("%s.%s = %s & %s.%s = %s.%s", treatment_alias,treatment_partition_col,blacklist_alias,blacklist_partition_col,treatment_alias, treatment_field, blacklist_alias, blacklist_field)
 		//val condition = s"${treatment_alias}.${treatment_field} = ${blacklist_alias}.${blacklist_field} AND  ${treatment_alias}.${treatment_partition_col} = ${blacklist_alias}.${blacklist_partition_col}" 
 		val condition = s"${treatment_alias}.${treatment_field} = ${blacklist_alias}.${blacklist_field} AND  ${treatment_alias}.${treatment_partition_col} = 'EU'" 
-
+		print (condition)
 		delta_table_1.alias(treatment_alias).merge(blacklist_table_df.alias(blacklist_alias), condition).whenMatched().delete().execute()
 		val end_time = Calendar.getInstance().getTimeInMillis()
 		val duration = end_time - start_time
@@ -42,6 +47,3 @@ object deletion_process_delta {
 	
 
 }
-
-
-
